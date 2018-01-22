e81402c4af0093f97070660d59c8743249218b91
Adam Mashrique <amashrique@gmail.com>
2018-01-22 14:36:21 +0800

    working getInfo display

--- web-ui/src/components/HelloWorld.vue
+++ web-ui/src/components/HelloWorld.vue
@@ -1,34 +1,18 @@
 <template>
   <div class="hello">
-    <el-row type="flex" class="row-bg" justify="left">
+    <el-row type="flex" class="row-bg" justify="center">
       <el-col :span="10">
         <div class="grid-content bg-purple-light">
-          <!-- <p>{{ msg }}</p> -->
-          <el-table
-            :data="tableData"
-            style="width: 100%">
-            <el-table-column
-              prop="date"
-              label="Date"
-              width="120">
-            </el-table-column>
-            <el-table-column
-              prop="name"
-              label="Name"
-              width="120">
-            </el-table-column>
-            <el-table-column
-              prop="address"
-              label="Address">
-            </el-table-column>
-          </el-table>
-
-          <textarea v-model="nodeInfo">
-          </textarea>
+          <el-form ref="form" label-width="120px">
+            <el-form-item v-for="(value, key) in nodeInfo" :label="key" v-bind:key="key">
+              <el-input :value="value.toString()" readonly></el-input>
+            </el-form-item>
+          </el-form>
+          <!-- DEBUG -->
+          <!-- <pre>{{ nodeInfo }}</pre> -->
         </div>
       </el-col>
     </el-row>
-
   </div>
 </template>
 
@@ -39,7 +23,7 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      nodeInfo: 'fgsfds',
+      nodeInfo: {},
       tableData: [
         {
           date: '2016-05-03',
@@ -53,7 +37,7 @@ export default {
     axios.get('http://hyve.ddns.net:3000/v1/getinfo')
       .then((response) => {
         // JSON responses are automatically parsed.
-        this.nodeInfo = response;
+        this.nodeInfo = response.data;
       })
       .catch((e) => {
         this.nodeInfo = e;
@@ -65,14 +49,14 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .bg-purple-light {
-  background: #e5e9f2;
+  /* background: #e5e9f2; */
 }
 .grid-content {
-  border-radius: 4px;
-  min-height: 36px;
+  /* border-radius: 4px; */
+  /* min-height: 36px; */
 }
 .row-bg {
-  padding: 10px;
-  background-color: #f9fafc;
+  padding: 1em;
+  /* background-color: #f9fafc; */
 }
 </style>
